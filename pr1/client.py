61b410c17780782aea23a2ee0b3daa72ac8e523b
Dmitry Kozerovsky <dima29koz@yandex.ru>
2022-12-12 16:23:28 +0300

    done pr1

--- pr1/client.py
+++ pr1/client.py
@@ -18,37 +18,41 @@ class Client:
         self.server_socket = None
         self.tr_server = None
         self.tr_choice = None
+        self.tr_server_health = None
         self.numbers = []
+        self.is_server = False
         if not self.check_server():
             self.send_cmd_to_rnd()
 
     def send_cmd_to_rnd(self):
-        print('sending gen command')
+        # print('sending gen command')
         self.client_socket.sendto('~gen_num'.encode('utf-8'), (self.MCAST_GRP, self.CLIENT_PORT))
 
     def send_number(self):
         self.numbers = []
         number = random.randint(0, 100)
-        print('sending number')
+        # print('sending number')
         self.client_socket.sendto(str(number).encode('utf-8'), (self.MCAST_GRP, self.CLIENT_PORT))
         self.tr_choice = threading.Thread(target=self.choice_host)
         self.tr_choice.start()
 
     def choice_host(self):
         sleep(0.5)
-        print('choosing proc start')
+        # print('choosing proc start')
         self.server_address_port = sorted(self.numbers, key=lambda item: item[0], reverse=True)[0][1]
-        print(self.server_address_port, self.ip)
         if self.ip == self.server_address_port:
             self.create_server()
             self.check_server()
         return
 
     def create_server(self):
+        self.is_server = True
         self.server_socket = self.create_socket(('', self.MCAST_PORT))
         self.tr_server = threading.Thread(target=self.server_loop)
+        self.tr_server_health = threading.Thread(target=self.server_health_loop)
         self.tr_server.start()
-        print('server started')
+        self.tr_server_health.start()
+        print('im server now!')
 
     def create_socket(self, address, timeout=False):
         listen_socket = socket.socket(
@@ -68,46 +72,59 @@ class Client:
         try:
             data, address = self.client_socket.recvfrom(self.BUFFER_SIZE)
         except TimeoutError:
-            print('server doesnt exist')
+            # print('server doesnt exist')
             return False
         else:
-            print('server exists')
-            print('data:', data.decode('utf-8'))
-            print('addr', address)
+            # print('server exists')
+            # print('data:', data.decode('utf-8'))
+            # print('addr', address)
             self.server_address_port = address
             return True
 
     def server_loop(self):
         while True:
             message, address = self.server_socket.recvfrom(self.BUFFER_SIZE)
-            print(f'Message from Client: {message.decode(encoding="utf-8")}', f'\nClient IP Address: {address}')
+            # print(f'Message from Client: {message.decode(encoding="utf-8")}', f'\nClient IP Address: {address}')
             self.server_socket.sendto(message, (self.MCAST_GRP, self.CLIENT_PORT))
+
+    def server_health_loop(self):
+        while True:
+            self.server_socket.sendto(b'~health check', (self.MCAST_GRP, self.CLIENT_PORT))
+            sleep(7)
 
     def send_msg(self, msg: bytes):
         self.client_socket.sendto(msg, self.server_address_port)
 
     def receive_msg(self):
         while True:
+            self.client_socket.settimeout(20)
             try:
                 data, address = self.client_socket.recvfrom(self.BUFFER_SIZE)
                 if data:
                     if data.decode('utf-8') == "~gen_num":
-                        print('received gen command')
+                        # print('received gen command')
                         self.send_number()
+                    elif data.decode('utf-8') == "~health check":
+                        continue
                     elif data.decode('utf-8').isnumeric():
-                        print('received number')
+                        # print('received number')
                         num = int(data.decode('utf-8'))
                         self.numbers.append((num, address))
                     else:
                         print(f'Message from Server: {data.decode("utf-8")}')
-            except:
+            except Exception as e:
+                print(e)
+                self.send_cmd_to_rnd()
                 continue
 
     def get_input(self):
         while True:
-            msg = input()
-            if msg:
-                self.send_msg(msg.encode('utf-8'))
+            try:
+                msg = input()
+                if msg:
+                    self.send_msg(msg.encode('utf-8'))
+            except UnicodeDecodeError:
+                break
 
     def run(self):
         tr_listen = threading.Thread(target=self.receive_msg)
@@ -117,6 +134,7 @@ class Client:
         tr_listen.join()
         tr_send.join()
         self.tr_server.join()
+        self.tr_server_health.join()
         self.tr_choice.join()
 
 
